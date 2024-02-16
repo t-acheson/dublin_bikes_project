df57f11dae8869d43c52c5f9ff449e88cff8613e
t-acheson <taraacheson@gmail.com>
2024-02-16 17:44:00 +0000

    framework for api requests & sql insertion started

---
+++ sql_commands.py
@@ -0,0 +1,20 @@
+#import stuff
+import requests
+
+
+#step 1 request data from api 
+
+#step 2 parse data into json if not already
+
+#step 3 connect to sql database
+
+#step 4 insert data into the database
+cursor = conn.cursor() #to make sql commands work i believe? 
+#make table for station number 
+#sql statememt to insert into table 
+#try, exepct
+
+#step 5 commit transaction?
+
+#step 6 close connect 
+conn.close()
