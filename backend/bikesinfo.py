6d257ef94d2fee48e28a7497fa8af6b7cbfa80c8
Ritwik Agarwal <ritwikagarwal11@gmail.com>
2024-02-20 10:06:18 +0000

    bikes scrapper and weather scrapper

---
+++ backend/bikesinfo.py
@@ -0,0 +1,5 @@
+
+class BikesConfig:
+    API_KEY = "6f6d1a36509d2b7893b73519d26a3f0c81aebfc1"
+    CONTRACT_NAME="dublin"
+    URI="https://api.jcdecaux.com/vls/v1/stations?contract="+CONTRACT_NAME+"&apiKey="+API_KEY+""
