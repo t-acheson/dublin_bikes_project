3efaed8eb4d76a0a1a77344fbbfb04bc645f1e43
t-acheson <taraacheson@gmail.com>
2024-02-22 10:37:14 +0000

    started adding sql inserter to the bike & weather scrapper

--- bikeScrapper.py
+++ bikeScrapper.py
@@ -26,19 +26,42 @@ def bikesToTables():
             data = json.loads(r.text)  # Use r.text instead of r.test
             store(data)  # Call the store function with the parsed data
 
-            #open db connection here 
+           #open db connection here 
             connection = mysql.connector.connect(
-                host ="localhost goes here"
-                database ="x"
-                user="y"
-                password="z"
+                host = "dublinbikes20.c9g2qa8qkqxt.eu-north-1.rds.amazonaws.com",
+                database = "dublinbikesgroup20",
+                user = "admin",
+                password = "dublinbikesgroup20",
             )
             
-            #sql commands go here 
-
-        except Exception as e:
-            print("error") #additional error handling to go here & eventually print into the no hang up file on EC2
+            # Create a cursor object to execute SQL commands
+            cursor = connection.cursor()
             
+            #TODO need to change the below code to reflect the relevant table 
+             # Loop over the data and insert each record into the database
+            # for record in data:
+            #     number = record['number']
+            #     name = record['name']
+            #     address = record['address']
+            #     banking = int(record['banking'])  # Convert boolean to int
+            #     bike_stands = record['bike_stands']
+            #     position_lat = record['position']['lat']
+            #     position_lng = record['position']['lng']
+
+            # # Construct the SQL command
+            #     sql = """
+            #     INSERT INTO station (number, name, address, banking, bike_stands, position_lat, position_lng)
+            #     VALUES (%s, %s, %s, %s, %s, %s, %s)
+            #     """
+                 
+            #     # Execute the SQL command
+            #     cursor.execute(sql, (number, name, address, banking, bike_stands, position_lat, position_lng))
+
+                # Commit the changes 
+                connection.commit()
+            # close the connection
+            connection.close()
+
 
         # Sleep for  5 minutes
         # time.sleep(5 *  60) #use cron its on ubuntu
--- finalWeatherScraper.py
+++ finalWeatherScraper.py
@@ -27,8 +27,46 @@ def weatherToTables():
             w = json.loads(weather_data.text)
             store(w)  # Call the store function with the parsed data
 
+
+ #open db connection here 
+            connection = mysql.connector.connect(
+                host = "dublinbikes20.c9g2qa8qkqxt.eu-north-1.rds.amazonaws.com",
+                database = "dublinbikesgroup20",
+                user = "admin",
+                password = "dublinbikesgroup20",
+            )
+            
+            # Create a cursor object to execute SQL commands
+            cursor = connection.cursor()
+            
+            #TODO change the following code to reflect the relevant table 
+            #  # Loop over the data and insert each record into the database
+            # for record in data:
+            #     number = record['number']
+            #     name = record['name']
+            #     address = record['address']
+            #     banking = int(record['banking'])  # Convert boolean to int
+            #     bike_stands = record['bike_stands']
+            #     position_lat = record['position']['lat']
+            #     position_lng = record['position']['lng']
+
+            # # Construct the SQL command
+            #     sql = """
+            #     INSERT INTO station (number, name, address, banking, bike_stands, position_lat, position_lng)
+            #     VALUES (%s, %s, %s, %s, %s, %s, %s)
+            #     """
+                 
+            #     # Execute the SQL command
+            #     cursor.execute(sql, (number, name, address, banking, bike_stands, position_lat, position_lng))
+
+            #     # Commit the changes 
+            #     connection.commit()
+            # close the connection
+            connection.close()
+
+
             # Sleep for  5 minutes
-            time.sleep(5 *  60) #use cron its on ubuntu
+            # time.sleep(5 *  60) #use cron its on ubuntu
            
 
         except Exception as e:
