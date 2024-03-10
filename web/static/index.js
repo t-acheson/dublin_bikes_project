de081cd810a762a6112b8c9a801fb5cfbfaaca25
Ritwik Agarwal <ritwikagarwal11@gmail.com>
2024-03-10 11:55:13 +0000

    map markers and info pop up

--- web/static/index.js
+++ web/static/index.js
@@ -20,6 +20,7 @@ async function initMap() {
     lat = 53.3498;
     lng = -6.2603;
   }
+
   const map = new google.maps.Map(document.getElementById("map"), {
     
     center: { lat:lat, lng:lng},
@@ -98,6 +99,8 @@ async function initMap() {
     
   });
 
+  const stationsData = await GetStationsData();
+  // const weatherData = await GetWeatherData();
   const input = document.getElementById("pac-input");            
   const autocomplete = new google.maps.places.Autocomplete(input);
   autocomplete.bindTo("bounds", map);
@@ -109,6 +112,74 @@ async function initMap() {
     }
     map.fitBounds(place.geometry.viewport);
   });
-  
+
+  //marker for current location
+  new google.maps.Marker({
+    position: {lat: lat, lng: lng},
+    map:map,
+    icon: {
+      url: 'https://img.icons8.com/color/48/marker--v1.png',
+      scaledSize: new google.maps.Size(35, 35),
+    }
+  });  
+
+  //marker for bike station locations
+  for(var i=0; i< stationsData.length; i++)
+  {
+    const marker = new google.maps.Marker({
+      position: stationsData[i].position,
+      map,
+      icon:{
+        url:"https://img.icons8.com/stickers/100/electric-bike.png",
+        scaledSize: new google.maps.Size(35,35)
+      }
+    });
+    AddInfoWindow(marker, map, stationsData[i]);
+  }
 }
+
+function AddInfoWindow(marker, map, markerData)
+{
+  const bikeStationInfo = 
+  ` <div class="stationsInfo">
+    <h3 class="infoHeading">${markerData.name}</h3>
+    <p class="info">Status: ${markerData.status}</p>
+    <p class="info">Available Bikes: ${markerData.available_bikes}</p>
+    <p class="info">Parking: ${markerData.available_bike_stands}</p>
+    <p class="info">Banking: ${markerData.banking ? "yes":"no"}</p>
+    </div>`
+
+  const infoWindow = new google.maps.InfoWindow({
+    content:bikeStationInfo
+  });
+
+  //displaying information of bike station when the user hovers over the marker on map
+  marker.addListener("mouseover", ()=>{
+    infoWindow.open({
+      anchor:marker,
+      map
+    });
+  });
+
+//closing information window when marker loses mouse focus
+  marker.addListener("mouseout", ()=>{
+    infoWindow.close();
+  })
+}
+
+async function GetStationsData()
+{
+  const bikePromise = await fetch("https://api.jcdecaux.com/vls/v1/stations?contract=dublin&apiKey=9923c4b16f8c5fd842f2f448564bed43a349fa47", {mode:"cors"})
+  bikesData = await bikePromise.json(); 
+  return bikesData;
+}
+
+async function GetWeatherData()
+{
+  const weatherPromise = await fetch("http://api.weatherapi.com/v1/current.json?key=0f5a8ade5f024e70a34123035241602&q=dublin",{mode:"cors"});
+  wData = await weatherPromise.json();
+  return wData;
+}
+
 initMap();
+setInterval(()=>initMap(),120000);
