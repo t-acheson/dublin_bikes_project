43c1687f91c7a8ea4e7300f8913a99138829c12d
Ritwik Agarwal <ritwikagarwal11@gmail.com>
2024-03-10 19:25:59 +0000

    Resolved errors pertaining to google maps loading and current location detection

--- web/static/index.js
+++ web/static/index.js
@@ -8,99 +8,87 @@ async function initMap() {
     maximumAge: 0,
   };
 
-  if(navigator.geolocation)
-  {
-    navigator.geolocation.getCurrentPosition((position)=>{
-      lat = parseFloat(position.coords.latitude);
-      lng = parseFloat(position.coords.longitude);
-    }, null, options);
-  }
-  else
-  {
-    lat = 53.3498;
-    lng = -6.2603;
-  }
-
-  const map = new google.maps.Map(document.getElementById("map"), {
-    
-    center: { lat:lat, lng:lng},
+  const {Map} = await google.maps.importLibrary("maps");
+  map = new Map(document.getElementById("map"), {
+    center: new google.maps.LatLng(53.3498, -6.2603),
     zoom: 13,
     disableDefaultUI: true,
     mapTypeControlOptions: {
       style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR,
       position: google.maps.ControlPosition.TOP_CENTER
     },
-    styles: [
-    {
-      "featureType": "all",
-      "elementType": "labels.text.fill",
-      "stylers": [{"color": "#ffffff"}]
-    },
-    {
-      "featureType": "all",
-      "elementType": "labels.text.stroke",
-      "stylers": [{"color": "#000000"},{"lightness": 13}]
-    },
-    {
-      "featureType": "administrative",
-      "elementType": "geometry.fill",
-      "stylers": [{"color": "#000000"}]
-    },
-    {
-      "featureType": "administrative",
-      "elementType": "geometry.stroke",
-      "stylers": [{"color": "#144b53"},{"lightness": 14},{"weight": 1.4}]
-    },
-    {
-      "featureType": "landscape",
-      "elementType": "all",
-      "stylers": [{"color": "#08304b"}]
-    },
-    {
-      "featureType": "poi",
-      "elementType": "geometry",
-      "stylers": [{"color": "#0c4152"},{"lightness": 5}]
-    },
-    {
-      "featureType": "road.highway",
-      "elementType": "geometry.fill",
-      "stylers": [{"color": "#000000"}]
-    },
-    {
-      "featureType": "road.highway",
-      "elementType": "geometry.stroke",
-      "stylers": [{"color": "#0b434f"},{"lightness": 25}]
-    },
-    {
-      "featureType": "road.arterial",
-      "elementType": "geometry.fill",
-      "stylers": [{"color": "#000000"}]
-    },
-    {
-      "featureType": "road.arterial",
-      "elementType": "geometry.stroke",
-      "stylers": [{"color": "#0b3d51"},{"lightness": 16}]
-    },
-    {
-      "featureType": "road.local",
-      "elementType": "geometry",
-      "stylers": [{"color": "#000000"}]
-    },
-    {
-      "featureType": "transit",
-      "elementType": "all",
-      "stylers": [{"color": "#146474"}]
-    },
-    {
-      "featureType": "water",
-      "elementType": "all",
-      "stylers": [{"color": "#021019"}]
-    }]
+    styles:[
+      {
+          "featureType": "all",
+          "stylers": [
+              {
+                  "saturation": 0
+              },
+              {
+                  "hue": "#e7ecf0"
+              }
+          ]
+      },
+      {
+          "featureType": "road",
+          "stylers": [
+              {
+                  "saturation": -70
+              }
+          ]
+      },
+      {
+          "featureType": "transit",
+          "stylers": [
+              {
+                  "visibility": "off"
+              }
+          ]
+      },
+      {
+          "featureType": "poi",
+          "stylers": [
+              {
+                  "visibility": "off"
+              }
+          ]
+      },
+      {
+          "featureType": "water",
+          "stylers": [
+              {
+                  "visibility": "simplified"
+              },
+              {
+                  "saturation": -60
+              }
+          ]
+      }
+  ]
     
   });
 
+  if(navigator.geolocation)
+  {
+    navigator.geolocation.getCurrentPosition((position)=>{
+        lat = parseFloat(position.coords.latitude);
+        lng = parseFloat(position.coords.longitude);
+        map.setCenter(new google.maps.LatLng(lat, lng));
+        //marker for current location
+        new google.maps.Marker({
+          position: new google.maps.LatLng(lat, lng),
+          map:map,
+          icon: {
+            url: 'https://img.icons8.com/color/48/marker--v1.png',
+            scaledSize: new google.maps.Size(35, 35),
+          }
+        });
+      }, null, options);
+  }
+
   const stationsData = await GetStationsData();
   // const weatherData = await GetWeatherData();
+
   const input = document.getElementById("pac-input");            
   const autocomplete = new google.maps.places.Autocomplete(input);
   autocomplete.bindTo("bounds", map);
@@ -113,24 +101,15 @@ async function initMap() {
     map.fitBounds(place.geometry.viewport);
   });
 
-  //marker for current location
-  new google.maps.Marker({
-    position: {lat: lat, lng: lng},
-    map:map,
-    icon: {
-      url: 'https://img.icons8.com/color/48/marker--v1.png',
-      scaledSize: new google.maps.Size(35, 35),
-    }
-  });  
-
+  
   //marker for bike station locations
   for(var i=0; i< stationsData.length; i++)
   {
     const marker = new google.maps.Marker({
-      position: stationsData[i].position,
+      position: new google.maps.LatLng(stationsData[i].position.lat, stationsData[i].position.lng),
       map,
       icon:{
-        url:"https://img.icons8.com/stickers/100/electric-bike.png",
+        url:"../static/bicycle-svgrepo-com.png",
         scaledSize: new google.maps.Size(35,35)
       }
     });
@@ -146,7 +125,7 @@ function AddInfoWindow(marker, map, markerData)
     <p class="info">Status: ${markerData.status}</p>
     <p class="info">Available Bikes: ${markerData.available_bikes}</p>
     <p class="info">Parking: ${markerData.available_bike_stands}</p>
-    <p class="info">Banking: ${markerData.banking ? "yes":"no"}</p>
+    <p class="info">Banking: ${markerData.banking ? "Yes":"No"}</p>
     </div>`
 
   const infoWindow = new google.maps.InfoWindow({
@@ -181,5 +160,19 @@ async function GetWeatherData()
   return wData;
 }
 
-initMap();
-setInterval(()=>initMap(),120000);
+async function GetLatAndLang(lat, lng)
+{
+  const options = {
+    enableHighAccuracy: true,
+    timeout: 5000,
+    maximumAge: 0,
+  };
+
+  if(navigator.geolocation)
+  {
+    navigator.geolocation.getCurrentPosition((position)=>{
+      lat = parseFloat(position.coords.latitude);
+      lng = parseFloat(position.coords.longitude);
+    }, null, options); 
+  }
+}
