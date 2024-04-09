8d6987c814b65e2d80dfaaad2ac56a19f641c406
Ritwik Agarwal <ritwikagarwal11@gmail.com>
2024-04-09 09:57:59 +0100

    Saving the journey planner implementation

--- web/static/index.js
+++ web/static/index.js
@@ -2,6 +2,7 @@ let map;
 let lat, lng;
 let previousStation = null;
 let directionsService , directionsRenderer;
+let source, destination;
 async function initMap() {
   const options = {
     enableHighAccuracy: true,
@@ -45,59 +46,49 @@ async function initMap() {
         ],
       },
       {
-          "featureType": "transit",
-          "stylers": [
-              {
-                  "visibility": "on"
-              }
-          ]
+        featureType: "transit",
+        stylers: [
+          {
+            visibility: "on",
+          },
+        ],
       },
       {
-          "featureType": "poi",
-          "stylers": [
-              {
-                  "visibility": "on"
-              }
-          ]
+        featureType: "poi",
+        stylers: [
+          {
+            visibility: "on",
+          },
+        ],
       },
       {
-          "featureType": "water",
-          "stylers": [
-              {
-                  "visibility": "simplified"
-              },
-              {
-                  "saturation": -60
-              }
-          ]
-      }
-  ]
-    
+        featureType: "water",
+        stylers: [
+          {
+            visibility: "simplified",
+          },
+          {
+            saturation: -60,
+          },
+        ],
+      },
+    ],
+  });
+
+  directionsService = new google.maps.DirectionsService();
+  directionsRenderer = new google.maps.DirectionsRenderer({map, panel: document.getElementById("panel")});
+
+  //initial marker marks the location which google maps will automatically assume as starting point for the user
+  var initialMarker = new google.maps.Marker({
+    position: new google.maps.LatLng(lat, lng),
+    map: map,
+    icon: {
+      url: "https://img.icons8.com/color/48/marker--v1.png",
+      scaledSize: new google.maps.Size(35, 35),
+    },
   });
 
   const stationsData = await GetStationsData();
-
-  if(navigator.geolocation)
-  {
-    navigator.geolocation.getCurrentPosition((position)=>{
-        lat = parseFloat(position.coords.latitude);
-        lng = parseFloat(position.coords.longitude);
-        map.setCenter(new google.maps.LatLng(lat, lng));
-        //marker for current location
-        new google.maps.Marker({
-          position: new google.maps.LatLng(lat, lng),
-          map:map,
-          icon: {
-            url: 'https://img.icons8.com/color/48/marker--v1.png',
-            scaledSize: new google.maps.Size(35, 35),
-          }
-        });
-        //TODO: please add the function to get 5 closest stations here for current location
-      }, null, options);
-  }
-  //const stationsData = {{ stations|tojson }};
-  
-  // const weatherData = await GetWeatherData();
 
   const input = document.getElementById("pac-input");
   const autocomplete = new google.maps.places.Autocomplete(input);
@@ -116,6 +107,11 @@ async function initMap() {
       return;
     }
 
+    //removing the marker for original location
+    initialMarker.setMap(null);
+    lat = place.geometry.location.lat();
+    lng = place.geometry.location.lng();
+
     //placing a marker at the searched location
     new google.maps.Marker({
       position: new google.maps.LatLng(
@@ -128,13 +124,18 @@ async function initMap() {
         scaledSize: new google.maps.Size(35, 35),
       },
     });
-
-    //TODO: please add the function to get 5 closest stations here for the searched location
+    //showing closest stations when user searches for it
+    findClosestStations(lat, lng, stationsData);
     map.fitBounds(place.geometry.viewport);
-     findClosestStations(place, bikesData);
   });
 
-   
+  //displaying the journey form when user clicks on the button
+  const journeyButton = document.getElementById("journeyPlanner");
+  journeyButton.addEventListener("click",()=>{
+    //show journey planner form
+    document.getElementById('travelForm').style.display = 'block';
+  });
+
   //marker for bike station locations
   for (var i = 0; i < stationsData.length; i++) {
     const marker = new google.maps.Marker({
@@ -150,6 +151,20 @@ async function initMap() {
     });
     AddInfoWindow(marker, map, stationsData[i]);
   }
+  FillStations(stationsData);
+
+  // Get the close button element
+  var closeButton = document.getElementById('close-button');
+ 
+  // Add a click event listener to the close button
+  closeButton.addEventListener('click', function() {
+    document.getElementById('popup-window').style.display = 'none';
+  });
+
+  var closeJourneyButton = document.getElementById("close");
+  closeJourneyButton.addEventListener("click",()=>{
+    document.getElementById("travelForm").style.display = 'none';
+  });
 }
 
 function AddInfoWindow(marker, map, markerData) {
@@ -179,195 +194,6 @@ function AddInfoWindow(marker, map, markerData) {
   });
 }
 
-async function GetStationsData() {
-  const bikePromise = await fetch(
-    "https://api.jcdecaux.com/vls/v1/stations?contract=dublin&apiKey=9923c4b16f8c5fd842f2f448564bed43a349fa47",
-    { mode: "cors" }
-  );
-  bikesData = await bikePromise.json();
-  return bikesData;
-}
-
-//function to find the 5 closest stations by lat, lng and return them in a list
-function FindClosestStations(lat, lng, stationsData) {
-  if (!stationsData) {
-    console.log("!!! stationsData is undefined or null !!!");
-    return; // Exit the function if stationsData is not valid
-  }
-  const stationList = [];
-
-  // Iterate over the stationsData object
-  Object.entries(stationsData).forEach(([stationId, stationData]) => {
-    let latDiff = stationData.position.lat - lat;
-    let lngDiff = stationData.position.lng - lng;
-    let calculatedDistance = Math.sqrt(latDiff * latDiff + lngDiff * lngDiff);
-
-    // Store the station data and its distance from the given latitude and longitude
-    stationList.push({ station: stationData, distance: calculatedDistance });
-  });
-
-  // Sort by distance and take the first 5 closest
-  stationList.sort((a, b) => a.distance - b.distance);
-  let closestStations = stationList.slice(0, 5).map((item) => item.station);
-
-  // Return the closest stations
-  return closestStations;
-}
-
-async function GetLatAndLang(lat, lng)
-{
-  const options = {
-    enableHighAccuracy: true,
-    timeout: 5000,
-    maximumAge: 0,
-  };
-
-  const {Map} = await google.maps.importLibrary("maps");
-  map = new Map(document.getElementById("map"), {
-    center: new google.maps.LatLng(53.3498, -6.2603),
-    zoom: 13,
-    disableDefaultUI: true,
-    mapTypeControlOptions: {
-      style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR,
-      position: google.maps.ControlPosition.TOP_CENTER
-    },
-    styles:[
-      {
-          "featureType": "all",
-          "stylers": [
-              {
-                  "saturation": 0
-              },
-              {
-                  "hue": "#e7ecf0"
-              }
-          ]
-      },
-      {
-          "featureType": "road",
-          "stylers": [
-              {
-                  "saturation": -70
-              }
-          ]
-      },
-      {
-          "featureType": "transit",
-          "stylers": [
-              {
-                  "visibility": "off"
-              }
-          ]
-      },
-      {
-          "featureType": "poi",
-          "stylers": [
-              {
-                  "visibility": "off"
-              }
-          ]
-      },
-      {
-          "featureType": "water",
-          "stylers": [
-              {
-                  "visibility": "simplified"
-              },
-              {
-                  "saturation": -60
-              }
-          ]
-      }
-  ]
-    
-  });
-
-//when user uses search bar ? //TODO need to clarify 
-  if(navigator.geolocation)
-  { 
-    navigator.geolocation.getCurrentPosition((position)=>{
-        lat = parseFloat(position.coords.latitude);
-        lng = parseFloat(position.coords.longitude);
-        map.setCenter(new google.maps.LatLng(lat, lng));
-       
-        //marker for current location
-        new google.maps.Marker({
-          position: new google.maps.LatLng(lat, lng),
-          map:map,
-          icon: {
-            url: 'https://img.icons8.com/color/48/marker--v1.png',
-            scaledSize: new google.maps.Size(35, 35),
-          }
-        });
-      }, null, options);
-  }
-
-  const stationsData = await GetStationsData();
-  // const weatherData = await GetWeatherData();
-
-  const input = document.getElementById("pac-input");            
-  const autocomplete = new google.maps.places.Autocomplete(input);
-  autocomplete.bindTo("bounds", map);
-  autocomplete.addListener("place_changed", () => {
-    const place = autocomplete.getPlace();
-    if (!place.geometry || !place.geometry.location) {
-        window.alert("No details available for input: '" + place.name + "'");
-        return;
-    }
-    map.fitBounds(place.geometry.viewport);
-  
-    findClosestStations(place, bikesData);
-  
-    
-  });
-
-  
-  //marker for bike station locations
-  for(var i=0; i< stationsData.length; i++)
-  {
-    const marker = new google.maps.Marker({
-      position: new google.maps.LatLng(stationsData[i].position.lat, stationsData[i].position.lng),
-      map,
-      icon:{
-        url:"../static/bicycle-svgrepo-com.png",
-        scaledSize: new google.maps.Size(35,35)
-      }
-    });
-    AddInfoWindow(marker, map, stationsData[i]);
-  }
-}
-
-function AddInfoWindow(marker, map, markerData)
-{
-  //TODO for i in stattions data let number = number in availability table & use that table instead 
-
-  const bikeStationInfo = 
-  ` <div class="stationsInfo">
-    <h3 class="infoHeading">${markerData.name}</h3>
-    <p class="info">Status: ${markerData.status}</p>
-    <p class="info">Available Bikes: ${markerData.available_bikes}</p>
-    <p class="info">Parking: ${markerData.available_bike_stands}</p>
-    <p class="info">Banking: ${markerData.banking ? "Yes":"No"}</p>
-    </div>`
-
-  const infoWindow = new google.maps.InfoWindow({
-    content:bikeStationInfo
-  });
-
-  //displaying information of bike station when the user hovers over the marker on map
-  marker.addListener("mouseover", ()=>{
-    infoWindow.open({
-      anchor:marker,
-      map
-    });
-  });
-
-//closing information window when marker loses mouse focus
-  marker.addListener("mouseout", ()=>{
-    infoWindow.close();
-  })
-}
-
 async function GetStationsData() //TODO change from api to db 
 {
   const bikePromise = await fetch("https://api.jcdecaux.com/vls/v1/stations?contract=dublin&apiKey=9923c4b16f8c5fd842f2f448564bed43a349fa47", {mode:"cors"})
@@ -375,40 +201,13 @@ async function GetStationsData() //TODO change from api to db
   return bikesData;
 }
 
-async function GetWeatherData() //TODO change to weather table not directly from weather API
-{
-  const weatherPromise = await fetch("http://api.weatherapi.com/v1/current.json?key=0f5a8ade5f024e70a34123035241602&q=dublin",{mode:"cors"});
-  wData = await weatherPromise.json();
-  return wData;
-}
-
-//TODO check what puurpose this is serving, could not access lat & lng from it, currently accessing within findClostestStations function which may be redundant?  
-async function GetLatAndLang(lat, lng)
-{
-  const options = {
-    enableHighAccuracy: true,
-    timeout: 5000,
-    maximumAge: 0,
-  };
-
-  if(navigator.geolocation)
-  {
-    navigator.geolocation.getCurrentPosition((position)=>{
-      lat = parseFloat(position.coords.latitude);
-      lng = parseFloat(position.coords.longitude);
-    }, null, options); 
-  }
-
-}
-
-
 //function to find the 5 closest stations by lat, lng and return them in a list 
- function findClosestStations(place, stationsData) {
+function findClosestStations(lat, lng, stationsData) {
   //finding lat & lng of user 
-  let location = place.geometry.location;
-  let lat = location.lat();
-  let lng = location.lng();
-  console.log("lat: " + lat ); //*testing purposes only 
+  // let location = place.geometry.location;
+  // let lat = location.lat();
+  // let lng = location.lng();
+  // console.log("lat: " + lat ); //*testing purposes only 
   // console.log("lng: " + lng); 
   const stationList = []; 
 
@@ -446,12 +245,9 @@ const closestStations = stationList.slice(0, 5);
   // Return the closest stations in pop up window 
   showPopup(closestStations);
  }
-//end of findClosestStation function 
-
 
 //popup for closest stations
 function showPopup(closestStations) {
-  console.log("pop up ")
   // Generate the content for the popup
   let content = '';
   closestStations.forEach(station => {
@@ -465,20 +261,105 @@ function showPopup(closestStations) {
   document.getElementById('popup-window').style.display = 'block';
  }
  
- // Get the close button element
- var closeButton = document.getElementById('close-button');
+ //This function gets the route from point A to point B using cycling as mode of transport
+ function GetRoute(sourceLat, sourceLng, destLat, destLng)
+ {
+   directionsRenderer.setMap(map);
  
- // Add a click event listener to the close button
- closeButton.addEventListener('click', function() {
-  document.getElementById('popup-window').style.display = 'none';
- });
+   var start = new google.maps.LatLng(sourceLat, sourceLng);
+   var end = new google.maps.LatLng(destLat, destLng);
  
+   var request = {
+     origin: start,
+     destination: end,
+     travelMode: 'BICYCLING'
+   };
+   directionsService.route(request, function(result, status) {
+     if (status == 'OK') {
+       directionsRenderer.setDirections(result);
+     }
+   });
+ 
+   const panel = document.getElementById('panel');
+   panel.style.display = "block";
+ }
+
 
 //journey planner functions start 
 
-//TODO get user input from start
+function FillStations(stationsData)
+{
+  const firstDropdown = document.getElementById("sourceDropdown");
+  const secondDropdown = document.getElementById("destDropdown");
 
-//TODO get user input from destination 
+  for(let i=0; i< stationsData.length; i++)
+  {
+    let opt = document.createElement("option");
+    opt.className = "travelOption";
+
+    opt.value = stationsData[i].name;
+    opt.text = stationsData[i].name;
+
+    //adding to first menu
+    firstDropdown.appendChild(opt);
+  }
+  
+  //similar code for second dropdown menu
+  for(let i=0; i< stationsData.length; i++)
+  {
+    let opt = document.createElement("option");
+    opt.className = "travelOption";
+
+    opt.value = stationsData[i].name;
+    opt.text = stationsData[i].name;
+
+    //adding to second menu
+    secondDropdown.appendChild(opt);
+  }
+
+  //recording the source and destination
+  firstDropdown.onchange = function(){
+    source = firstDropdown.value;
+  }
+
+  secondDropdown.onchange = function(){
+    destination = secondDropdown.value;
+  }
+
+  const showRouteButton = document.getElementById("showRoute");
+
+  showRouteButton.addEventListener("click", ()=>{
+    PlanJourney(source, destination, stationsData);
+    document.getElementById("travelForm").style.display = 'none';
+  });
+}
+
+function PlanJourney(source, destination, stationsData)
+{
+  var sourceLat, sourceLng, destLat, destLng;
+
+  for(let i=0 ; i< stationsData.length; i++)
+  {
+    if(source == stationsData[i].name && source!= "Source")
+    {
+      sourceLat = stationsData[i].position.lat;
+      sourceLng = stationsData[i].position.lng;
+    }
+
+    if(destination == stationsData[i].name && destination != "Destination") 
+    {
+      destLat = stationsData[i].position.lat;
+      destLng = stationsData[i].position.lng;
+    }
+  }
+
+  //we will get the route only when we have both locations
+  if(sourceLat && sourceLng && destLat && destLng)
+  {
+    GetRoute(sourceLat, sourceLng, destLat, destLng);
+  }
+}
+
 
 //TODO get user input from time choice 
 
