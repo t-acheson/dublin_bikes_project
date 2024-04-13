2a03d85f45c56f75f5a6a004febcad751f923bb3
jackb2024 <150268817+jackb2024@users.noreply.github.com>
2024-04-13 15:05:02 +0100

    predict function now working. calling correctly

--- web/predict.py
+++ web/predict.py
@@ -8,21 +8,16 @@ from flask import jsonify
 
 
 def predict(stationid, temp_c, wind_mph, precip_mm, hours):
-    # station = 2
-    # Load the model
 
-    filename = f'./mlModel/model_{stationid}.pkl' # Replaces {station} with the actual station ID
+    filename = f'./web/mlModel/model_{stationid}.pkl' # Replaces {station} with the actual station ID
     with open(filename, 'rb') as file:
         model = pickle.load(file)
 
-    # Prepare the input data for prediction
-    # Assuming you have a DataFrame df_prediction with the same columns as X_train
-    # For example, let's say you want to predict for a specific hour
     df_prediction = pd.DataFrame({
-        'temp_c': [temp_c], # Example temperature
-        'wind_mph': [wind_mph], # Example wind speed
-        'precip_mm': [precip_mm], # Example precipitation
-        'hours': [hours] # Example hour
+        'temp_c': [temp_c], 
+        'wind_mph': [wind_mph],
+        'precip_mm': [precip_mm],
+        'hours': [hours] 
     })
 
     # Predict the number of available bikes
